452b854f8329637a059a9278e239b2a48ad16785
Suyi32 <lisuyi_imagine@outlook.com>
2022-01-04 18:34:47 +0800

    init

---
+++ go.mod
@@ -0,0 +1,40 @@
+module faas-container-idler
+
+go 1.17
+
+require (
+	github.com/prometheus/client_model v0.2.0
+	github.com/prometheus/common v0.32.1
+	k8s.io/apimachinery v0.20.2
+	k8s.io/client-go v0.20.2
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v0.2.0 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/googleapis/gnostic v0.4.1 // indirect
+	github.com/imdario/mergo v0.3.5 // indirect
+	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0 // indirect
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
+	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c // indirect
+	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.26.0-rc.1 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/api v0.20.2 // indirect
+	k8s.io/klog/v2 v2.4.0 // indirect
+	k8s.io/utils v0.0.0-20201110183641-67b214c5f920 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.0.2 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"time"
+	// "flag"
+	"net/http"
+	"os"
+	"fmt"
+	"log"
+	"context"
+	// "io/ioutil"
+	"io"
+
+	dto "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
+	"k8s.io/client-go/rest"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	// "k8s.io/client-go/tools/clientcmd"
+)
+
+const metricPort = 8081
+
+func main() {
+	log.Println("Start running faas container idler.")
+
+	/*
+	kubeconfig := flag.String("kubeconfig", "/home/suyi/.kube/config", "location to kubeconfig file")
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+    if err != nil {
+        panic(err.Error())
+    }
+	*/
+
+	
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates k8s clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	
+	podMap := make(map[string][]int64)
+
+	reconcileInterval := time.Second * 10000
+
+	client := &http.Client{}
+	inactivityDurationVal := os.Getenv("inactivity_duration")
+
+	if len(inactivityDurationVal) == 0 {
+		inactivityDurationVal = "5m"
+	}
+
+	inactivityDuration, _ := time.ParseDuration(inactivityDurationVal)
+
+	for {
+		reconcile(client, clientset, inactivityDuration, podMap)
+		time.Sleep(reconcileInterval)
+		log.Printf("\n")
+	}
+}
+
+
+
+func reconcile(client *http.Client, clientset *kubernetes.Clientset, inactivityDuration time.Duration, podMap map[string][]int64) {
+	endpoints, err := clientset.CoreV1().Endpoints("openfaas-fn").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	
+	for _, endpoint := range endpoints.Items{
+		funcName := endpoint.ObjectMeta.Name
+		// fmt.Println("endpointName: ", funcName)
+		// fmt.Println("endpoint level: ", ep_idx , endpoint)
+		// fmt.Println("subset level: ", ep_idx , endpoint.Subsets)
+		for _, subset := range endpoint.Subsets{
+			// fmt.Println("address level: ", add_idx ,add.Addresses)
+			for _, address := range(subset.Addresses) {
+				log.Println("IP level: " , address.IP, address.TargetRef.Name)
+				metricURL := fmt.Sprintf("http://%s:%d/metrics", address.IP, metricPort)
+				resp, err := client.Get(metricURL)
+				if err != nil {
+					panic(err.Error())
+				}
+				defer resp.Body.Close()
+
+				mf, err := parseMF(resp.Body)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+
+				var total 	 float64
+				var inflight float64 
+
+				totalKey := "http_requests_total"
+				totalMetrics, found := mf[totalKey] 
+				if !found {
+					total = 0.0
+				} else {
+					total = totalMetrics.Metric[0].GetCounter().GetValue()
+				}
+
+				inflightKey := "http_requests_in_flight"
+				inflightMetrics, found := mf[inflightKey]
+				if !found {
+					log.Println("http_requests_in_flight not found.")
+				} else {
+					inflight = inflightMetrics.Metric[0].GetCounter().GetValue()
+				}
+
+				log.Println(totalKey, total)
+				log.Println(inflightKey, inflight)
+				getReplicas(funcName, clientset)
+
+				history, found := podMap[address.TargetRef.Name]
+				if !found {
+					podMap[address.TargetRef.Name] = make([]int64, 2)
+					podMap[address.TargetRef.Name][0] = int64(total)
+					podMap[address.TargetRef.Name][1] = time.Now().Unix()
+				} else {
+					ifRemove := getIfRemove(int64(total), int64(inflight), history)
+					if ifRemove {
+						log.Println("Start Removing idle containers: ", address.TargetRef.Name)
+						/*
+						1. Save logs
+						2. Label deleteCost
+						3. ScaleDownByOne
+						*/
+						scaleDownbyOne(funcName, clientset)
+					}
+				}
+
+			}
+		}
+	}
+	
+	
+}
+
+func scaleDownbyOne(funcName string, clientset *kubernetes.Clientset) {
+	deployment, err := clientset.AppsV1().Deployments("openfaas-fn").Get(context.TODO(), funcName, metav1.GetOptions{})
+	if err != nil {
+        panic(err.Error())
+    }
+	oldReplicas := *deployment.Spec.Replicas
+	newReplicas := int32(oldReplicas - 1)
+
+	log.Printf("Set replicas - %s %s, %d -> %d\n", funcName, "openfaas-fn", oldReplicas, newReplicas)
+	
+	deployment.Spec.Replicas = &newReplicas
+	_, err = clientset.AppsV1().Deployments("openfaas-fn").Update(context.TODO(), deployment, metav1.UpdateOptions{})
+	if err != nil {
+		log.Println(err)
+		panic(err.Error())
+	}
+}
+
+func getReplicas(funcName string, clientset *kubernetes.Clientset) {
+	deployment, err := clientset.AppsV1().Deployments("openfaas-fn").Get(context.TODO(), funcName, metav1.GetOptions{})
+	if err != nil {
+        panic(err.Error())
+    }
+	Replicas := *deployment.Spec.Replicas
+	log.Println("Func: ", funcName, "Replica: ", Replicas)
+}
+
+
+func getIfRemove(total int64, inflight int64, history []int64) bool {
+	if inflight == 0 {
+		if history[0] == total {
+			idleDuration := time.Now().Unix() - history[1]
+			if idleDuration >= 300 {
+				return true
+			} else {
+				return false
+			}
+		} else {
+			history[0] = total
+			history[1] = time.Now().Unix()
+			return false
+		}
+	} else {
+		history[1] = time.Now().Unix()
+		return false
+	}
+
+	return false
+}
+
+
+func parseMF(reader io.Reader) (map[string]*dto.MetricFamily, error) {
+    var parser expfmt.TextParser
+    mf, err := parser.TextToMetricFamilies(reader)
+    if err != nil {
+        return nil, err
+    }
+    return mf, nil
+}
